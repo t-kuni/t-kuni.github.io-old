6625b86fd8aac9c2e1a4ee6594233e0999da6ec2
kunita <kunita@waltsinc.jp>
2019-08-06 14:51:31 +0900

    install vue-router, vuex

---
+++ src/components/pages/TopPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>help</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {},
+    mounted() {
+    },
+    props     : {},
+    data      : function () {
+      return {
+      }
+    },
+    computed  : {
+    },
+    methods   : {
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import TopPage from "./components/pages/TopPage";
+
+export default [
+    {
+        path     : '/',
+        name     : 'top',
+        component: TopPage
+    },
+];
